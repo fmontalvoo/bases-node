473a7afc0094f114a76323ebf94c7aa634ec9855
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-16 17:32:51 -0500

    Configuracion yargs.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const { generarTabla, generarTablas } = require('./src/mult');
+const argv = require('yargs')
+    .option('number', {
+        type: 'number',
+        alias: 'n',
+        demandOption: true,
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.number))
+            throw 'Debe ingresar un número';
+        return true;
+    })
+    .argv;
 
 console.clear();
 
-const [, , arg3 = 'num=7'] = process.argv;
-const [, num] = arg3.split('=');
-const n = parseInt(num);
+// const [, , arg3 = 'num=7'] = process.argv;
+// const [, num] = arg3.split('=');
+// const n = parseInt(num);
 
-generarTablas(n)
+generarTablas(argv.number)
     .then(res => {
         if (res)
             console.info('Archivo creado');
